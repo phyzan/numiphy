bdb4369e85f1f10e74fabc74995c03ecd6340f72
senpy <fivoszan@gmail.com>
2025-08-06 01:33:39 +0300

    small changes in lowlevel odes

--- numiphy/odesolvers/orbits.py
+++ numiphy/odesolvers/orbits.py
@@ -137,7 +137,7 @@ class HamiltonianSystem2D(OdeSystem):
         return cls._process_args(obj, f, t, *q, args=args, events=events)
     
 
-    def get(self, t0: float, q0: np.ndarray, *, rtol=1e-6, atol=1e-12, min_step=0., max_step=np.inf, first_step=0., args=(), method="RK45"):
+    def get_orbit(self, t0: float, q0: np.ndarray, *, rtol=1e-6, atol=1e-12, min_step=0., max_step=np.inf, first_step=0., args=(), method="RK45"):
         if len(q0) != self.Nsys:
             raise ValueError(f"The size of the initial conditions provided is {len(q0)} instead of {self.Nsys}")
         return HamiltonianOrbit2D(self.lowlevel_odefunc, self.lowlevel_jac, t0=t0, q0=q0, rtol=rtol, atol=atol, min_step=min_step, max_step=max_step, first_step=first_step, args=args, method=method, events=self.lowlevel_events)
@@ -162,7 +162,7 @@ class HamiltonianVariationalSystem2D(HamiltonianSystem2D):
         return cls._process_args(obj, new_sys, t, *[*q, *delq], args=args, events=events)
     
 
-    def get_variational(self, t0: float, q0: np.ndarray, period: float, *, rtol=1e-6, atol=1e-12, min_step=0., max_step=np.inf, first_step=0., args=(), method="RK45")->VariationalLowLevelODE:
+    def get_variational_orbit(self, t0: float, q0: np.ndarray, period: float, *, rtol=1e-6, atol=1e-12, min_step=0., max_step=np.inf, first_step=0., args=(), method="RK45"):
         if len(q0) != self.Nsys:
             raise ValueError(f"The size of the initial conditions provided is {len(q0)} instead of {self.Nsys}")
         return HamiltonianVariationalOrbit2D(self.lowlevel_odefunc, self.lowlevel_jac, t0=t0, q0=q0, period=period, rtol=rtol, atol=atol, min_step=min_step, max_step=max_step, first_step=first_step, args=args, method=method, events=self.lowlevel_events)
